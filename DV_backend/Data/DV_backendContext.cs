463d1aa69bca74002fd8ad923e0245e07b183d4f
Владя <DVORA02@MAIL.RU>
2023-02-21 11:13:20 +0300

    hello world

---
+++ DV_backend/Data/DV_backendContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace DV_backend.Data
+{
+    public class DV_backendContext : DbContext
+    {
+        public DV_backendContext (DbContextOptions<DV_backendContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ DV_backend/Data/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DV_backend.Data
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
